1113301bbdb52c3937f363718791f7739458367e
Enriquez-Johnmark <enriquezjohnmark15@gmail.com>
2024-02-15 15:10:07 +0800

    add env files, add auth routes and modify index js file

---
+++ api/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import "dotenv/config";
+import authRoutes from "./routes/auth.routes.js"
+
+const app = express();
+
+
+
+const PORT = process.env.PORT || 5000;
+app.get("/", (req,res) => {
+res.send("Hello World!");
+});
+
+
+app.use("/api/auth", authRoutes);
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
---
+++ api/routes/auth.routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+
+
+router.get("/login", (req,res) => {
+    res.send("Hello Login");
+});
+
+export default router;
